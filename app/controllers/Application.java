b37f557068a36b611f1f066cad31c33f23231d0d
matsfunk <mathias.funk@gmail.com>
2015-10-07 11:11:20 +0200

    added dashboard draft added styling from materialize.css improved logging

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,22 +4,44 @@ import java.io.IOException;
 
 import model.WebSocketClient;
 import nl.tue.id.oocsi.server.OOCSIServer;
+import play.Logger;
 import play.mvc.Controller;
+import play.mvc.Result;
 import play.mvc.WebSocket;
 
 public class Application extends Controller {
 
 	private static OOCSIServer server = null;
 
-	public static WebSocket<String> index() {
+	public WebSocket<String> index() {
 		WebSocketClient wsc = new WebSocketClient(getServer(), request().remoteAddress());
 		return wsc.getHandler();
+
+		// return WebSocket.whenReady((in, out) -> {
+		// // For each event received on the socket,
+		// in.onMessage(System.out::println);
+		//
+		// // When the socket is closed.
+		// in.onClose(() -> System.out.println("Disconnected"));
+		//
+		// // Send a single 'Hello!' message
+		// out.write("Hello!");
+		// });
+	}
+
+	public Result dashboard() {
+		return ok(views.html.Application.dashboard.render("title", "content"));
 	}
 
 	private static OOCSIServer getServer() {
 		if (server == null) {
 			try {
-				server = new OOCSIServer(4444, 1000, false);
+				server = new OOCSIServer(4444, 1000, false) {
+					@Override
+					protected void internalLog(String message) {
+						Logger.info(message);
+					}
+				};
 			} catch (IOException e) {
 				// do nothing for now
 				e.printStackTrace();
