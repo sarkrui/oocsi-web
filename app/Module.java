75a22175102473c2944915abffd7bd9e2cf1868f
matsfunk <mathias.funk@gmail.com>
2020-04-17 07:50:27 +0200

    v0.3.7  - added new version of underlying OOCSI socket server library  - improved the data canvas tool

--- app/Module.java
+++ app/Module.java
@@ -9,6 +9,12 @@ public class Module extends AbstractModule {
 	@Override
 	protected void configure() {
 		try {
+			// debug restart
+			OOCSIServer os = OOCSIServer.getInstance();
+			if (os != null) {
+				os.destroyInstance();
+			}
+
 			bind(OOCSIServer.class).toInstance(new OOCSIServer(4444, 1000, true) {
 				@Override
 				protected void internalLog(String message) {
